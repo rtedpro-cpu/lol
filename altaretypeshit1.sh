88058e201c23ef92e9fcbfbd871b886f7fd87983
imm0nv1nhtv <111904644+imm0nv1nhtv@users.noreply.github.com>
2025-03-26 16:32:58 +0700

    Create altaretypeshit1.sh

---
+++ altaretypeshit1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eo pipefail
+echo "Installing uDocker..."
+curl -O https://github.com/indigo-dc/udocker/releases/download/1.3.17/udocker-1.3.17.tar.gz
+tar zxvf udocker-1.3.17.tar.gz
+export PATH=`pwd`/udocker-1.3.17/udocker:$PATH
+export FILEDIR=`pwd`/udocker-1.3.17/udocker
+mv $FILEDIR/udocker $FILEDIR/op
+# udocker being udocker..
+sed -i '1s|#!/usr/bin/env python|#!/usr/bin/env python3|' `pwd`/udocker-1.3.17/udocker/op
+op install
+# Setting execmode to runc
+export UDOCKER_DEFAULT_EXECUTION_MODE=R1
+# Fix runc execution issue
+export XDG_RUNTIME_DIR=$HOME
+echo "Installing the Debian container..."
+op pull debian
+op create --name=debian debian
+op setup --execmode=R1 debian
+
+cat > start_container.sh << EOF
+
+#!/bin/sh
+export XDG_RUNTIME_DIR=$HOME
+export PATH=`pwd`/udocker-1.3.17/udocker:$PATH
+op setup --execmode=R1 debian
+op run debian /bin/bash
+EOF
+chmod +x start_container.sh
+echo "Setup complete. You can now run the container with: ./start_container.sh"
+echo "Script will auto destroy."
+rm "$0"
