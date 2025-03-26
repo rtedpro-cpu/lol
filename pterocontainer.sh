38147eeb9812818e94b876cd3464679a1dfb47cf
imm0nv1nhtv <111904644+imm0nv1nhtv@users.noreply.github.com>
2025-03-26 17:24:29 +0700

    Update pterocontainer.sh

--- pterocontainer.sh
+++ pterocontainer.sh
@@ -14,20 +14,20 @@ mv $FILEDIR/udocker $FILEDIR/op
 sed -i '1s|#!/usr/bin/env python|#!/usr/bin/env python3|' `pwd`/udocker-1.3.17/udocker/op
 op install
 # Setting execmode to runc
-export UDOCKER_DEFAULT_EXECUTION_MODE=P2
+export UDOCKER_DEFAULT_EXECUTION_MODE=P1
 export UDOCKER_USE_PROOT_EXECUTABLE=$(which zsh)
 echo "Installing the Debian container..."
 op pull debian
 op create --name=debian debian
-op setup --execmode=P2 debian
+op setup --execmode=P1 debian
 
 cat > start_container.sh << EOF
 
 #!/bin/sh
 export PATH=`pwd`/udocker-1.3.17/udocker:$PATH
 export UDOCKER_USE_PROOT_EXECUTABLE=$(which zsh)
-op setup --execmode=P2 debian
-op run --containerauth debian /bin/bash
+op setup --execmode=P1 debian
+op run --containerauth --nosysdirs --bindhome debian /bin/bash
 EOF
 chmod +x start_container.sh
 echo "Setup complete. You can now run the container with: ./start_container.sh"
