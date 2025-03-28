69ff4445a0fad2fd91d90e9919d3bad16c5eef6d
imm0nv1nhtv <111904644+imm0nv1nhtv@users.noreply.github.com>
2025-03-28 19:12:47 +0700

    Update pterocontainer.sh

--- pterocontainer.sh
+++ pterocontainer.sh
@@ -27,7 +27,7 @@ cat > start_container.sh << EOF
 export PATH=`pwd`/udocker-1.3.17/udocker:$PATH
 export UDOCKER_USE_PROOT_EXECUTABLE=$(which zsh)
 op setup --execmode=P1 ubuntu
-op run --containerauth --bindhome --workdir /root --publish=$(SERVER_PORT):2222 ubuntu /bin/bash
+op run --containerauth --bindhome --workdir /root --publish=$SERVER_PORT:2222 ubuntu /bin/bash
 EOF
 chmod +x start_container.sh
 echo "Setup complete. You can now run the container with: ./start_container.sh"
